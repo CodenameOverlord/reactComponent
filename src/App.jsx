d4b8b0002c9528eb604776694961eb45219415d9
CodenameOverlord <utsavkumardatta@gmail.com>
2024-10-01 02:24:43 +0530

    dynamic component in react how to

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 function HeaderAdd(){
-    return (      <header>
+  
+    return (      
+    <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {
+          reactDescription[getRandomInt(reactDescription.length-1)]
+        }
+         React concepts you will need for almost any app you are
         going to build!
         Something extra
       </p>
-    </header>);
+    </header>
+    );
+}
+
+const reactDescription = ["Fundamental ", "Crucial ", "Core "];
+function getRandomInt(max){
+  return Math.floor(Math.random()*(max+1));
 }
 
 function App() {
