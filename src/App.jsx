163ae1c5bf1fe1402296956d9950003ea03cc9b4
CodenameOverlord <utsavkumardatta@gmail.com>
2024-10-01 02:25:55 +0530

    dynamic component - better practise

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 function HeaderAdd(){
-  
+  const description = reactDescription[getRandomInt(reactDescription.length-1)];
     return (      
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {
-          reactDescription[getRandomInt(reactDescription.length-1)]
+          // reactDescription[getRandomInt(reactDescription.length-1)]
+          description
         }
          React concepts you will need for almost any app you are
         going to build!
