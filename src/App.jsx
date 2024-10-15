5c1f9fc1ae2984ac4fdd680c45da3a8042ae630b
CodenameOverlord <utsavkumardatta@Utsavs-MacBook-Air.local>
2024-10-15 20:05:40 +0530

    props basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 function HeaderAdd(){
   const description = reactDescription[getRandomInt(reactDescription.length-1)];
@@ -26,11 +27,31 @@ function getRandomInt(max){
   return Math.floor(Math.random()*(max+1));
 }
 
+function CoreConcept (props){
+  return (
+  <li>
+    <img src={componentsImg} alt={componentsImg} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+
+  </li>)
+}
+
 function App() {
   return (
     <div>
       <HeaderAdd />
       <main>
+        <section>
+          <h2>Core Concepts 1</h2>
+          <ul>
+            <CoreConcept
+            title = "Components"
+            description = "The core UI building block"
+            image = {componentsImg}
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
