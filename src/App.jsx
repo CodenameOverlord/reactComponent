0872cac5d4f3b970e7e9e919cc8496bb15c71a29
CodenameOverlord <utsavkumardatta@Utsavs-MacBook-Air.local>
2024-10-16 10:23:27 +0530

    code rectification

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function getRandomInt(max){
 function CoreConcept (props){
   return (
   <li>
-    <img src={componentsImg} alt={componentsImg} />
+    <img src= {props.image} alt={props.title} />
     <h3>{props.title}</h3>
     <p>{props.description}</p>
 
@@ -47,9 +47,9 @@ function App() {
           <h2>Core Concepts 1</h2>
           <ul>
             <CoreConcept {...CORE_CONCEPTS[0]}/>            
-            <CoreConcept {...CORE_CONCEPTS[0]}/>            
-            <CoreConcept {...CORE_CONCEPTS[0]}/>            
-            <CoreConcept {...CORE_CONCEPTS[0]}/>            
+            <CoreConcept {...CORE_CONCEPTS[1]}/>            
+            <CoreConcept {...CORE_CONCEPTS[2]}/>            
+            <CoreConcept {...CORE_CONCEPTS[3]}/>            
           </ul>
         </section>
         <h2>Time to get started!</h2>
