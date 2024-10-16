0945ba1c6701d5767ac34c6e407c2db7865b2464
CodenameOverlord <utsavkumardatta@Utsavs-MacBook-Air.local>
2024-10-16 09:44:09 +0530

    using data from array core_components exported from data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 function HeaderAdd(){
   const description = reactDescription[getRandomInt(reactDescription.length-1)];
@@ -42,13 +43,28 @@ function App() {
     <div>
       <HeaderAdd />
       <main>
-        <section>
+        <section id = "core-concepts">
           <h2>Core Concepts 1</h2>
           <ul>
             <CoreConcept
-            title = "Components"
-            description = "The core UI building block"
-            image = {componentsImg}
+            title = {CORE_CONCEPTS[0].title}
+            description = {CORE_CONCEPTS[0].description}
+            image = {CORE_CONCEPTS[0].image}
+            />            
+            <CoreConcept
+            title = {CORE_CONCEPTS[1].title}
+            description = {CORE_CONCEPTS[1].description}
+            image = {CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+            title = {CORE_CONCEPTS[2].title}
+            description = {CORE_CONCEPTS[2].description}
+            image = {CORE_CONCEPTS[2].image}
+            />            
+            <CoreConcept
+            title = {CORE_CONCEPTS[3].title}
+            description = {CORE_CONCEPTS[3].description}
+            image = {CORE_CONCEPTS[3].image}
             />
           </ul>
         </section>
