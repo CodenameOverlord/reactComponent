5a9f7a65ec58a6ea3b4493ba36edcd70663e6379
CodenameOverlord <utsavkumardatta@Utsavs-MacBook-Air.local>
2024-10-19 17:56:52 +0530

    components added to different structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
-import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
+import HeaderAdd from './components/Header.jsx'
+import { CoreConcept } from './components/CoreConcepts.jsx';
 
-function HeaderAdd(){
-  const description = reactDescription[getRandomInt(reactDescription.length-1)];
-    return (      
-    <header>
-      <img src= {reactImg}
-      // "src/assets/react-core-concepts.png" 
-      alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {
-          // reactDescription[getRandomInt(reactDescription.length-1)]
-          description
-        }
-         React concepts you will need for almost any app you are
-        going to build!
-        Something extra
-      </p>
-    </header>
-    );
-}
 
-const reactDescription = ["Fundamental ", "Crucial ", "Core "];
-function getRandomInt(max){
-  return Math.floor(Math.random()*(max+1));
-}
 
-function CoreConcept ({image, title, description}){
-  return (
-  <li>
-    <img src= {image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
 
-  </li>)
-}
 
 function App() {
   return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import reactImg from '../assets/react-core-concepts.png';
+
+
+export default function HeaderAdd(){
+    const description = reactDescription[getRandomInt(reactDescription.length-1)];
+      return (      
+      <header>
+        <img src= {reactImg}
+        // "src/assets/react-core-concepts.png" 
+        alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {
+            // reactDescription[getRandomInt(reactDescription.length-1)]
+            description
+          }
+           React concepts you will need for almost any app you are
+          going to build!
+          Something extra
+        </p>
+      </header>
+      );
+  }
+
+const reactDescription = ["Fundamental ", "Crucial ", "Core "];
+function getRandomInt(max){
+  return Math.floor(Math.random()*(max+1));
+}
